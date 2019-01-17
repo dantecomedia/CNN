b892621a8c746c578abdd9f9b50aa606621ae941
RAHUL KUMAR <rahul97inc@gmail.com>
2019-01-17 23:20:15 +0530

    Create catvsdog.py

---
+++ catvsdog.py
@@ -0,0 +1,25 @@
+from keras.models import Sequential 
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classifier=Sequential()
+
+classifier.add(Convolution2D(32,3,3,input_shape=(64,64,3),activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+classifier.add(Flatten())
+
+classifier.add(Dense(output_dim=128,activation='relu'))
+classifier.add(Dense(output_dim=1,activation='sigmoid'))
+
+classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+#flow_from_directory(directory, target_size=(256, 256), color_mode='rgb', classes=None, class_mode='categorical', batch_size=32, shuffle=True, seed=None, save_to_dir=None, save_prefix='', save_format='png', follow_links=False, subset=None, interpolation='nearest')
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen=ImageDataGenerator(rescale=1/.255,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
+test_datagen=ImageDataGenerator(rescale=1/.255)
+training_set=train_datagen.flow_from_directory(r'''G:\Convolutional_Neural_Networks\Convolutional_Neural_Networks\dataset\training_set''',target_size=(64,64),batch_size=32,class_mode='binary')
+test_set=test_datagen.flow_from_directory(r'''G:\Convolutional_Neural_Networks\Convolutional_Neural_Networks\dataset\test_set''',target_size=(64,64),batch_size=32,class_mode='binary')
+
+
+classifier.fit_generator(training_set,samples_per_epoch=8000,nb_epoch=25,validation_data=test_set,nb_val_samples=2000)
